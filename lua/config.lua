e972bb38890f535290cd32cebcd44decbb01af76
Roddyck <rodion.ivashchenko2003@gmail.com>
2026-05-17 01:03:26 +0300

    chore: remove unused always_force_del option, update luadocs for config.options

--- lua/config.lua
+++ lua/config.lua
@@ -3,12 +3,13 @@ local M = {}
 
 ---@class packview.options
 ---@field window packview.options.window
----@field always_force_del boolean Always pass `force=true` to |vim.pack.del()| when deleting a package
 M.options = {
   ---@class packview.options.window
   ---@field width number Percentage of screen width for floating window as a fraction of total width
   ---@field height number Percentage of screen height for floating window as a fraction of total height
   ---@field border string|string[] Border style for floating window (same as passed to |nvim_open_win()|)
+  ---@field title string Title for floating window
+  ---@field title_pos string Position of title for floating window (same as passed to |nvim_open_win()|)
   window = {
     width = 0.8,
     height = 0.8,
@@ -16,8 +17,6 @@ M.options = {
     title = "Packview",
     title_pos = "center",
   },
-
-  always_force_del = false,
 }
 
 ---@param opts packview.options?
